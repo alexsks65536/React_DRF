33b243831a96c16d3f4fb0a6fd395f9733f0c2c9
alexsks65536 <megbyte@mail.ru>
2021-12-12 18:57:52 +0300

    lesson_03

---
+++ lesson_03/mainapp/models.py
@@ -0,0 +1,32 @@
+from decimal import Decimal
+from django.db import models
+from django.db.models.fields import DecimalField, FloatField, IntegerField
+from django.db.models.fields.related import ForeignKey
+
+# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name='имя', max_length=64, unique=True)
+    description = models.TextField(verbose_name='описание', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(verbose_name='имя', max_length=128, unique=True)
+    description = models.TextField(verbose_name='описание', blank=True, null=True)
+    category = ForeignKey(ProductCategory, on_delete=models.SET_NULL, null=True)
+    short_description = models.CharField(verbose_name='короткое описание',
+                                         max_length=64,
+                                         blank=True)
+    price = models.DecimalField(verbose_name='цена продукта',
+                         max_digits=8,
+                         decimal_places=2,
+                         default=0)
+    price_with_discount = models.DecimalField(verbose_name='цена со скидкой',
+                         max_digits=8,
+                         decimal_places=2,
+                         default=0)
+    quantity = models.PositiveIntegerField(verbose_name='Количество на складе',
+                         default=0)
+    image = models.ImageField(upload_to='products_images', blank=True)
---
+++ lesson_03/mainapp/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from .models import ProductCategory, Product
+
+
+def main(request):
+    title = 'главная'
+
+    products = Product.objects.all()[:4]
+
+    content = {'title': title, 'products': products, 'menu_links': menu_links}
+    return render(request, 'mainapp/index.html', content)
+
+# Create your views here.
+
+menu_links = [
+    {'href': '/', 'name': 'домой'},
+    {'href': '/products', 'name': 'продукты'},
+    {'href': '/contact', 'name': 'контакты'},
+]
+
+
+def products(request):
+
+    products = Product.objects.all()
+    productCategory = ProductCategory.objects.all()
+    return render(request, 'mainapp/products.html', context={
+        'menu_links': menu_links,
+        'products': products,
+        'productCategory': productCategory
+    })
+
+
+def contact(request):
+
+    return render(request, 'mainapp/contact.html', context={
+        'menu_links': menu_links,
+    })
+
+
